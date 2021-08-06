ecaf0675715a93388e5109f25966889e698fc722
Trevor Ewen <trevor@stg.software>
2021-08-06 16:25:37 -0400

    iFrame sample.

--- server/common/app.js
+++ server/common/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
+const cors = require('cors');
 const routes = require('./routes');
 const genericController = require('../api/controllers/generic');
 
 const app = express();
-app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.removeHeader('X-Frame-Options');
+  res.setHeader('X-Frame-Options', `Allow-From ${req.url}`);
   next();
 });
 
