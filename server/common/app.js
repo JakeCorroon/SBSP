60778be3cec89df325b25afa04e29e6969f4446a
Trevor Ewen <trevor@stg.software>
2021-08-06 16:18:02 -0400

    X frame policy allowed.

--- server/common/app.js
+++ server/common/app.js
@@ -10,6 +10,10 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.removeHeader('X-Frame-Options');
+  next();
+});
 
 routes.forEach(({ route, router }) => app.use(`/${route}`, router));
 
